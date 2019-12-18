eb279cbd9949fd8740770581b3c4cdaa05c75bdf
KariFra <mystery.tomorrow.2@gmail.com>
2019-12-18 21:24:02 +0100

    new changes

--- src/sample/Hand.java
+++ src/sample/Hand.java
@@ -1,46 +1,77 @@
 package sample;
 
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 
 
 public class Hand {
 
-    Image man = new Image("file:src/sample/assets/MAN.png");
-    Image woman = new Image("file:src/sample/assets/WOMAN.png");
-    Image knife = new Image("file:src/sample/assets/KNIFE.png");
-    Image gun = new Image("file:src/sample/assets/GUN.png");
-    Image inside = new Image("file:src/sample/assets/INSIDE.png");
-    Image outside = new Image("file:src/sample/assets/OUTSIDE.png");
-    Image day = new Image("file:src/sample/assets/DAY.png");
-    Image night = new Image("file:src/sample/assets/NIGHT.png");
 
-    CaseFiles caseFiles = new CaseFiles();
+
+    public void displayHand(){
+        Stage handCards = new Stage();
+        handCards.setTitle("Your deck");
+        handCards.setMinWidth(300);
+        handCards.initModality(Modality.APPLICATION_MODAL);
+
+
+        Rectangle card1 = new Rectangle(260.00, 70.00);
+        Rectangle card2 = new Rectangle(260.00, 70.00);
+        Rectangle card3 = new Rectangle(260.00, 70.00);
+
+        card1.setFill(new ImagePattern(new Image("file:src/sample/assets/"+handDeck.get(0).getPicture()+".png")));
+        card2.setFill(new ImagePattern(new Image("file:src/sample/assets/"+handDeck.get(1).getPicture()+".png")));
+        card3.setFill(new ImagePattern(new Image("file:src/sample/assets/"+handDeck.get(2).getPicture()+".png")));
+
+        VBox layout = new VBox();
+        layout.getChildren().addAll(card1,card2,card3);
+
+        Scene scene = new Scene(layout,300.00, 300.00, Color.BLACK);
+        handCards.setScene(scene);
+        handCards.show();
+    }
+
+
     ArrayList<Evidence> handDeck = new ArrayList<>();
     Evidence murderCase;
+    CaseFiles caseFiles;
 
-    
+    public Hand(CaseFiles caseFiles) {
+        this.caseFiles = caseFiles;
 
-
-    public void BeginTheGame(){
     }
-//    public Evidence chooseTheMurderCase(){
-//        caseFiles.pickPossibleCaseCards();
-//        murderCase = caseFiles.pickPossibleCaseCards().get(0);
-//        caseFiles.removeTheCaseFilesFromTheDeck(murderCase);
-//        return murderCase;
-//
+
+    public ArrayList<Evidence> BeginTheGameWithThreeCards() {
+        for (int i = 0; i < 2; i++) {
+            handDeck.add(caseFiles.pickTheCardFromTheDeck());
+            caseFiles.removeTheCardFromTheDeck(caseFiles.pickTheCardFromTheDeck());
+        }
+        return handDeck;
+    }
+    public Evidence chooseTheMurderCase(){
+        caseFiles.pickPossibleCaseCards();
+        murderCase = caseFiles.pickPossibleCaseCards().get(0);
+        caseFiles.removeTheCaseFilesFromTheDeck(murderCase);
+        return murderCase;
+
+    }
+
+//    public void pickTheCardDuringGame(){
+//        Evidence card1 =caseFiles.pickTheCardFromTheDeck();
+//        caseFiles.removeTheCardFromTheDeck(card1);
+//        handDeck.add(card1);
 //    }
-
-    public void pickTheCardDuringGame(){
-        Evidence card1 =caseFiles.pickTheCardFromTheDeck();
-        caseFiles.removeTheCardFromTheDeck(card1);
-        handDeck.add(card1);
-    }
 
     public void askForEvidenceComparison(){
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,12 +4,14 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -19,15 +21,12 @@ public class Main extends Application {
 
     Image picture = new Image("file:src/sample/assets/background.png");
    Image unknown = new Image("file:src/sample/assets/UNKNOWN.png");
-    Image img_button01 = new Image("file:src/sample/assets/button01.png");
-    Image img_button2 = new Image("file:src/sample/assets/button2.png");
-    Image img_button3 = new Image("file:src/sample/assets/button3.png");
     Image man = new Image("file:src/sample/assets/MAN.png");
     Image woman = new Image("file:src/sample/assets/WOMAN.png");
     Image knife = new Image("file:src/sample/assets/KNIFE.png");
     Image gun = new Image("file:src/sample/assets/GUN.png");
-    Image inside = new Image("file:src/sample/assets/INSIDE.png");
-    Image outside = new Image("file:src/sample/assets/OUTSIDE.png");
+    Image house = new Image("file:src/sample/assets/HOUSE.png");
+    Image garden = new Image("file:src/sample/assets/GARDEN.png");
     Image day = new Image("file:src/sample/assets/DAY.png");
     Image night = new Image("file:src/sample/assets/NIGHT.png");
 
@@ -36,18 +35,49 @@ public class Main extends Application {
 
 //creating objects
     CaseFiles caseFiles = new CaseFiles();
-    Hand hand = new Hand();
+    Hand hand = new Hand(caseFiles);
     Button button01 = new Button("     0/1     ");
     Button button2 = new Button("      2     ");
     Button button3 = new Button("      3     ");
-    Button button = new Button("Case");
+    Button button = new Button("Get the Case");
     Button buttonDraw = new Button("Draw a card");
+    Button buttonCheckYourHand = new Button("Choose card");
 
-    Rectangle caseCard = new Rectangle(250.00, 60.00);
+
+    Rectangle caseCard = new Rectangle(260.00, 70.00);
+
+
+
+    Rectangle card1 = new Rectangle(260.00, 70.00);
+    Rectangle card2 = new Rectangle(260.00, 70.00);
+    Rectangle card3 = new Rectangle(260.00, 70.00);
+    Rectangle card4 = new Rectangle(260.00, 70.00);
+    Rectangle card5 = new Rectangle(260.00, 70.00);
+    Rectangle card6 = new Rectangle(260.00, 70.00);
+    Rectangle card7 = new Rectangle(260.00, 70.00);
+    Rectangle card8 = new Rectangle(260.00, 70.00);
+    Rectangle card9 = new Rectangle(260.00, 70.00);
+    Rectangle card10 = new Rectangle(260.00, 70.00);
+    Rectangle card11 = new Rectangle(260.00, 70.00);
+    Rectangle card12 = new Rectangle(260.00, 70.00);
     Circle circle1 = new Circle(40.00);
     Circle circle2 = new Circle(40.00);
     Circle circle3 = new Circle(40.00);
     Circle circle4 = new Circle(40.00);
+
+
+    Rectangle cardUp1 = new Rectangle(260.00, 70.00);
+    Rectangle cardUp2 = new Rectangle(260.00, 70.00);
+    Rectangle cardUp3 = new Rectangle(260.00, 70.00);
+    Rectangle cardUp4 = new Rectangle(260.00, 70.00);
+    Rectangle cardUp5 = new Rectangle(260.00, 70.00);
+    Rectangle cardUp6 = new Rectangle(260.00, 70.00);
+    Rectangle cardUp7 = new Rectangle(260.00, 70.00);
+    Rectangle cardUp8 = new Rectangle(260.00, 70.00);
+    Rectangle cardUp9 = new Rectangle(260.00, 70.00);
+    Rectangle cardUp10 = new Rectangle(260.00, 70.00);
+    Rectangle cardUp11 = new Rectangle(260.00, 70.00);
+    Rectangle cardUp12 = new Rectangle(260.00, 70.00);
 
     Stage window;
 
@@ -65,12 +95,7 @@ public class Main extends Application {
         Background background = new Background(backgroundImage);
 
 
-//Preparing the grid
-        GridPane grid = new GridPane();
-        grid.setBackground(background);
-        grid.setPadding(new Insets(10.00, 10.00, 10.00, 10.00));
-        grid.setVgap(8.00);
-        grid.setHgap(10.00);
+
 
         circle1.setFill(new ImagePattern(unknown));
         circle2.setFill(new ImagePattern(unknown));
@@ -82,25 +107,23 @@ public class Main extends Application {
         buttonDraw.setVisible(false);
 
 
+//Preparing the grid
+        GridPane grid = new GridPane();
+        grid.setBackground(background);
+        grid.setPadding(new Insets(10.00, 5.00, 10.00, 10.00));
+        grid.setVgap(8.00);
+        grid.setHgap(10.00);
+
+
+
+
+
 
 //inserting elements to the grid
 
         Main main = new Main();
         main.fillTheGrid(grid);
 
-        GridPane.setConstraints(button, 7, 13);
-        GridPane.setConstraints(button01, 1, 13);
-        GridPane.setConstraints(button2, 3, 13);
-        GridPane.setConstraints(button3, 5, 13);
-        GridPane.setConstraints(buttonDraw, 7, 11);
-        GridPane.setConstraints(caseCard, 1, 1);
-        GridPane.setConstraints(circle1, 7, 9);
-        GridPane.setConstraints(circle2, 7, 7);
-        GridPane.setConstraints(circle3, 7, 5);
-        GridPane.setConstraints(circle4, 7, 3);
-
-
-        grid.getChildren().addAll(button,button01,button2, button3,buttonDraw, circle1, circle2, circle3, circle4,caseCard);
 
 
         // showing the cards
@@ -120,22 +143,19 @@ public class Main extends Application {
             caseFiles.mixTheDeck();
             System.out.println(caseFiles.newDeck);
             caseFiles.pickPossibleCaseCards();
-            System.out.println(caseFiles.newCaseFileCards);
-            System.out.println(caseFiles.newCaseFileCards.get(0));
-            System.out.println("file:src/sample/assets/"+caseFiles.newCaseFileCards.get(0).getPicture()+".png)");
-            caseFiles.removeTheCaseFilesFromTheDeck(caseFiles.newCaseFileCards.get(0));
-//            hand.chooseTheMurderCase();
-            caseCard.setFill(new ImagePattern(new Image("file:src/sample/assets/"+caseFiles.newCaseFileCards.get(0).getPicture()+".png)")));
+            Evidence murderCase = hand.chooseTheMurderCase();
+            caseFiles.removeTheCaseFilesFromTheDeck(murderCase);
+            caseCard.setFill(new ImagePattern(new Image("file:src/sample/assets/"+murderCase.getPicture()+".png")));
             caseCard.setVisible(true);
-//            System.out.println(caseFiles.newCard);
+            hand.BeginTheGameWithThreeCards();
         });
 
         buttonDraw.setOnAction((click) -> {
             caseFiles.pickTheCardFromTheDeck();
             if (caseFiles.newCard.getPlace() == "GARDEN" ){
-                circle1.setFill(new ImagePattern(outside));}
+                circle1.setFill(new ImagePattern(garden));}
             if (caseFiles.newCard.getPlace() == "HOUSE" ){
-                circle1.setFill(new ImagePattern(inside));}
+                circle1.setFill(new ImagePattern(house));}
             if (caseFiles.newCard.getTool() == "GUN" ){
                 circle2.setFill(new ImagePattern(gun));}
             if (caseFiles.newCard.getTool() == "KNIFE" ){
@@ -149,15 +169,16 @@ public class Main extends Application {
             if (caseFiles.newCard.getTime() == "NIGHT" ){
                 circle4.setFill(new ImagePattern(night));};
         });
+        buttonCheckYourHand.setOnAction((click) ->
+                hand.displayHand());
 
-
-        Scene scene = new Scene(grid, 900.00, 700.00, Color.BLACK);
-        this.window.setTitle("Crime Game");
-        this.window.setScene(scene);
-        this.window.show();
+        Scene scene = new Scene(grid, 1200.00, 1000.00, Color.BLACK);
+        window.setTitle("Crime Game");
+        window.setScene(scene);
+        window.show();
     }
     public void fillTheGrid (GridPane grid){
-        for(int i=0; i<8; i++){
+        for(int i=0; i<9; i++){
             ColumnConstraints column = new ColumnConstraints();
             column.setPercentWidth(50);
             grid.getColumnConstraints().add(column);
@@ -167,6 +188,55 @@ public class Main extends Application {
             row.setPercentHeight(10);
             grid.getRowConstraints().add(row);
         }
+
+        StackPane stack1 = new StackPane();
+        stack1.getChildren().addAll(new Rectangle(100,30,Color.WHITE), new Label("0/1"));
+        StackPane stack2 = new StackPane();
+        stack2.getChildren().addAll(new Rectangle(100,30,Color.WHITE), new Label("2"));
+        StackPane stack3 = new StackPane();
+        stack3.getChildren().addAll(new Rectangle(100,30,Color.WHITE), new Label("3"));
+
+        GridPane.setConstraints(button, 7, 13);
+        GridPane.setConstraints(button01, 1, 13);
+        GridPane.setConstraints(button2, 3, 13);
+        GridPane.setConstraints(button3, 5, 13);
+        GridPane.setConstraints(buttonDraw, 7, 11);
+        GridPane.setConstraints(buttonCheckYourHand,7,10);
+        GridPane.setConstraints(caseCard, 6, 13);
+        GridPane.setConstraints(circle1, 7, 9);
+        GridPane.setConstraints(circle2, 7, 7);
+        GridPane.setConstraints(circle3, 7, 5);
+        GridPane.setConstraints(circle4, 7, 3);
+        GridPane.setConstraints(card1, 0, 11);
+        GridPane.setConstraints(card2, 2, 11);
+        GridPane.setConstraints(card3, 4, 11);
+        GridPane.setConstraints(card4, 0, 10);
+        GridPane.setConstraints(card5, 2, 10);
+        GridPane.setConstraints(card6, 4, 10);
+        GridPane.setConstraints(card7, 0, 9);
+        GridPane.setConstraints(card8, 2, 9);
+        GridPane.setConstraints(card9, 4, 9);
+        GridPane.setConstraints(card10, 0, 8);
+        GridPane.setConstraints(card11, 2, 8);
+        GridPane.setConstraints(card12, 4, 8);
+        GridPane.setConstraints(cardUp1, 0, 1);
+        GridPane.setConstraints(cardUp2, 2, 1);
+        GridPane.setConstraints(cardUp3, 4, 1);
+        GridPane.setConstraints(cardUp4, 0, 2);
+        GridPane.setConstraints(cardUp5, 2, 2);
+        GridPane.setConstraints(cardUp6, 4, 2);
+        GridPane.setConstraints(cardUp7, 0, 3);
+        GridPane.setConstraints(cardUp8, 2, 3);
+        GridPane.setConstraints(cardUp9, 4, 3);
+        GridPane.setConstraints(cardUp10, 0, 4);
+        GridPane.setConstraints(cardUp11, 2, 4);
+        GridPane.setConstraints(cardUp12, 4, 4);
+        GridPane.setConstraints(stack1, 1, 0);
+        GridPane.setConstraints(stack2, 3, 0);
+        GridPane.setConstraints(stack3, 5, 0);
+
+
+        grid.getChildren().addAll(stack1,stack2,stack3,buttonCheckYourHand,button,button01,button2, button3, circle1, circle2, circle3, circle4,caseCard, card1, card2, card3, card4,card5,card6,card7,card8,card9,card10,card11,card12,cardUp1,cardUp2,cardUp3,cardUp4,cardUp5,cardUp6,cardUp7,cardUp8,cardUp9,cardUp10,cardUp11,cardUp12);
 
     }
 }
